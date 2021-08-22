46932822718436ac9b5c12f39cc9299d15de3946
InaYarashevich <ina.yarashevich@gmail.com>
2021-08-22 23:02:03 +0300

    new ex7

--- ex7.py
+++ ex7.py
@@ -3,11 +3,9 @@ import requests
 response1 = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type')
 print(response1.text)
 print(response1.status_code)
-
 response2 = requests.head('https://playground.learnqa.ru/ajax/api/compare_query_type', data={"method": "HEAD"})
 print(response2.text)
 print(response2.status_code)
-
 response3 = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type', params={"method": "GET"})
 print(response3.text)
 print(response3.status_code)
