510938d6198ad1dfad3fc165ecb024a360c10b7f
InaYarashevich <ina.yarashevich@gmail.com>
2021-08-23 13:47:29 +0300

    new+ ex5

--- ex5.py
+++ ex5.py
@@ -3,7 +3,11 @@ import json
 json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 obj = json.loads(json_text)
 jsonData = obj["messages"]
+key = "And this is a second message"
 for i in jsonData:
     values = i.values()
-    print(values)
+    lists = list(values)
+    print(lists[0])
+
+
 
