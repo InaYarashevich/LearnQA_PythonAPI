bb549fa6627d9da9ef080ed17b118b7e9cdbe0fa
InaYarashevich <ina.yarashevich@gmail.com>
2021-08-23 13:29:36 +0300

    new ex5

--- ex5.py
+++ ex5.py
@@ -2,8 +2,8 @@ import json
 
 json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 obj = json.loads(json_text)
-key = "And this is a second message"
-if key in json_text:
-    print(key)
-else:
-    print(f"Ключа {key} в JSON нет")
+jsonData = obj["messages"]
+for i in jsonData:
+    values = i.values()
+    print(values)
+
