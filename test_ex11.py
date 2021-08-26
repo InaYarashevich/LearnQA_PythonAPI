5b3017b464f2cc7e0d08ea85f693e1c56eb4d2e8
InaYarashevich <ina.yarashevich@gmail.com>
2021-08-26 19:29:37 +0300

    task 11 with changes

--- test_ex11.py
+++ test_ex11.py
@@ -9,8 +9,9 @@ class TestEx11:
         assert response.status_code == 200, "Wrong response code"
         print(dict(response.cookies))
 
-        cookie = "HomeWork"
-        cookie_value = response.cookies.get('HomeWork')
-        
-        assert cookie in response.cookies, "There is no such cookie"
-        assert cookie_value == "hw_value", "There is no such cookie value"
+        expected_cookie = "HomeWork"
+        expected_cookie_value = "hw_value"
+        actual_cookie_value = response.cookies.get('HomeWork')
+
+        assert expected_cookie in response.cookies, "There is no such cookie"
+        assert actual_cookie_value == expected_cookie_value, "There is no such cookie value"
