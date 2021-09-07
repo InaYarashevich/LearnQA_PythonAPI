55eb02c4bdb8e367f9774641d2bcbbb24a3a877d
InaYarashevich <ina.yarashevich@gmail.com>
2021-09-08 00:38:25 +0300

    Task 17

---
+++ tests/test_ex17.py
@@ -0,0 +1,183 @@
+import requests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+
+class TestUserEdit(BaseCase):
+    def test_edit_user_not_auth(self):
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        user_id = self.get_json_value(response1, "id")
+
+        # EDIT
+        new_name = "New Name"
+        response3 = requests.put(
+            f"https://playground.learnqa.ru/api/user/{user_id}",
+            headers=None,
+            cookies=None,
+            data={"firstName": new_name}
+        )
+        Assertions.assert_code_status(response3, 200)
+
+        # GET
+        response4 = requests.get(
+            f"https://playground.learnqa.ru/api/user/{user_id}",
+            headers=None,
+            cookies=None
+        )
+        Assertions.assert_json_value_by_name(
+            response4,
+            "firstName",
+            new_name,
+            "Wrong name of the user after edit"
+        )
+
+    def test_edit_other_user(self):
+        # REGISTER
+        data = self.prepare_registration_data()
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+        password = data['password']
+        email = data['email']
+        user_id = self.get_json_value(response, "id")
+
+        # LOGIN AS OTHER USER
+        data1 = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data1)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        # EDIT REGISTERED USER BY AUTH USER
+        new_name = "Changed Name"
+        response3 = requests.put(
+            f"https://playground.learnqa.ru/api/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": new_name}
+        )
+        Assertions.assert_code_status(response3, 200)
+
+        # LOGIN AS REGISTERED USER
+        login_data_of_edited_user = {
+            'email': email,
+            'password': password
+        }
+        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data_of_edited_user)
+
+        auth_sid1 = self.get_cookie(response2, "auth_sid")
+        token1 = self.get_header(response2, "x-csrf-token")
+
+        # CHECK EDITED PARAM
+        response4 = requests.get(
+            f"https://playground.learnqa.ru/api/user/{user_id}",
+            headers={"x-csrf-token": token1},
+            cookies={"auth_sid": auth_sid1}
+        )
+
+        Assertions.assert_json_value_by_name(
+            response4,
+            "firstName",
+            new_name,
+            "Wrong name of the user after edit"
+        )
+
+    def test_edit_user_wrong_email(self):
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # EDIT
+        new_email = "test_email.example.com"
+        response3 = requests.put(
+            f"https://playground.learnqa.ru/api/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"email": new_email}
+        )
+        Assertions.assert_code_status(response3, 200)
+
+        # GET
+        response4 = requests.get(
+            f"https://playground.learnqa.ru/api/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+        Assertions.assert_json_value_by_name(
+            response4,
+            "email",
+            new_email,
+            "Wrong name of the user after edit"
+        )
+
+    def test_edit_user_short_name(self):
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # EDIT
+        new_name = "Q"
+        response3 = requests.put(
+            f"https://playground.learnqa.ru/api/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": new_name}
+        )
+        Assertions.assert_code_status(response3, 200)
+
+        # GET
+        response4 = requests.get(
+            f"https://playground.learnqa.ru/api/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+        Assertions.assert_json_value_by_name(
+            response4,
+            "firstName",
+            new_name,
+            "Wrong name of the user after edit"
+        )
